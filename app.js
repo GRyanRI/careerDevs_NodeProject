a37022e4be83e733b6727ec05daf2018c333f062
GRyanRI <gregorywryan@gmail.com>
2020-02-04 13:30:19 -0500

    starting work on navbar

--- app.js
+++ app.js
@@ -5,19 +5,27 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: ['Books', 'Authors'],
+      title: 'Library'
+    }
+  );
 });
 
-app.listen(3000, function () {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
